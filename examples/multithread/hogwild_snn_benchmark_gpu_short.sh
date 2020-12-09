625fa55234b1d9564495f3b303b7432e291b283b
Osaze Shears <oshears@vt.edu>
2020-12-09 15:33:00 -0500

    updated scripts

--- examples/multithread/hogwild_snn_benchmark_gpu_short.sh
+++ examples/multithread/hogwild_snn_benchmark_gpu_short.sh
@@ -1,21 +1,21 @@
 #!/usr/bin/bash
 
-python benchmark.py --device gpu --n_threads 0 --batch_size 128 --rate_coding
-python benchmark.py --device gpu --n_threads 1 --batch_size 128 --rate_coding
-python benchmark.py --device gpu --n_threads 2 --batch_size 128 --rate_coding
-python benchmark.py --device gpu --n_threads 3 --batch_size 128 --rate_coding
-python benchmark.py --device gpu --n_threads 4 --batch_size 128 --rate_coding
-python benchmark.py --device gpu --n_threads 5 --batch_size 128 --rate_coding
-python benchmark.py --device gpu --n_threads 6 --batch_size 128 --rate_coding
-python benchmark.py --device gpu --n_threads 7 --batch_size 128 --rate_coding
-python benchmark.py --device gpu --n_threads 8 --batch_size 128 --rate_coding
+python hogwild_snn_benchmark.py --device gpu --n_threads 0 --batch_size 128 --rate_coding
+python hogwild_snn_benchmark.py --device gpu --n_threads 1 --batch_size 128 --rate_coding
+python hogwild_snn_benchmark.py --device gpu --n_threads 2 --batch_size 128 --rate_coding
+python hogwild_snn_benchmark.py --device gpu --n_threads 3 --batch_size 128 --rate_coding
+python hogwild_snn_benchmark.py --device gpu --n_threads 4 --batch_size 128 --rate_coding
+python hogwild_snn_benchmark.py --device gpu --n_threads 5 --batch_size 128 --rate_coding
+python hogwild_snn_benchmark.py --device gpu --n_threads 6 --batch_size 128 --rate_coding
+python hogwild_snn_benchmark.py --device gpu --n_threads 7 --batch_size 128 --rate_coding
+python hogwild_snn_benchmark.py --device gpu --n_threads 8 --batch_size 128 --rate_coding
 
-python benchmark.py --device gpu --n_threads 0 --batch_size 128 --temporal_coding
-python benchmark.py --device gpu --n_threads 1 --batch_size 128 --temporal_coding
-python benchmark.py --device gpu --n_threads 2 --batch_size 128 --temporal_coding
-python benchmark.py --device gpu --n_threads 3 --batch_size 128 --temporal_coding
-python benchmark.py --device gpu --n_threads 4 --batch_size 128 --temporal_coding
-python benchmark.py --device gpu --n_threads 5 --batch_size 128 --temporal_coding
-python benchmark.py --device gpu --n_threads 6 --batch_size 128 --temporal_coding
-python benchmark.py --device gpu --n_threads 7 --batch_size 128 --temporal_coding
-python benchmark.py --device gpu --n_threads 8 --batch_size 128 --temporal_coding
+python hogwild_snn_benchmark.py --device gpu --n_threads 0 --batch_size 128 --temporal_coding
+python hogwild_snn_benchmark.py --device gpu --n_threads 1 --batch_size 128 --temporal_coding
+python hogwild_snn_benchmark.py --device gpu --n_threads 2 --batch_size 128 --temporal_coding
+python hogwild_snn_benchmark.py --device gpu --n_threads 3 --batch_size 128 --temporal_coding
+python hogwild_snn_benchmark.py --device gpu --n_threads 4 --batch_size 128 --temporal_coding
+python hogwild_snn_benchmark.py --device gpu --n_threads 5 --batch_size 128 --temporal_coding
+python hogwild_snn_benchmark.py --device gpu --n_threads 6 --batch_size 128 --temporal_coding
+python hogwild_snn_benchmark.py --device gpu --n_threads 7 --batch_size 128 --temporal_coding
+python hogwild_snn_benchmark.py --device gpu --n_threads 8 --batch_size 128 --temporal_coding
--- examples/multithread/hogwild_snn_benchmark_short.sh
+++ examples/multithread/hogwild_snn_benchmark_short.sh
@@ -1,25 +1,25 @@
 #!/usr/bin/bash
 
-python benchmark.py --device cpu --n_threads 0 --batch_size 128 --rate_coding
-python benchmark.py --device cpu --n_threads 4 --batch_size 128 --rate_coding
-python benchmark.py --device cpu --n_threads 8 --batch_size 128 --rate_coding
-python benchmark.py --device cpu --n_threads 12 --batch_size 128 --rate_coding
-python benchmark.py --device cpu --n_threads 16 --batch_size 128 --rate_coding
-python benchmark.py --device cpu --n_threads 20 --batch_size 128 --rate_coding
-python benchmark.py --device cpu --n_threads 24 --batch_size 128 --rate_coding
-python benchmark.py --device cpu --n_threads 28 --batch_size 128 --rate_coding
-python benchmark.py --device cpu --n_threads 32 --batch_size 128 --rate_coding
-python benchmark.py --device cpu --n_threads 36 --batch_size 128 --rate_coding
-python benchmark.py --device cpu --n_threads 40 --batch_size 128 --rate_coding
+python hogwild_snn_benchmark.py --device cpu --n_threads 0 --batch_size 128 --rate_coding
+python hogwild_snn_benchmark.py --device cpu --n_threads 4 --batch_size 128 --rate_coding
+python hogwild_snn_benchmark.py --device cpu --n_threads 8 --batch_size 128 --rate_coding
+python hogwild_snn_benchmark.py --device cpu --n_threads 12 --batch_size 128 --rate_coding
+python hogwild_snn_benchmark.py --device cpu --n_threads 16 --batch_size 128 --rate_coding
+python hogwild_snn_benchmark.py --device cpu --n_threads 20 --batch_size 128 --rate_coding
+python hogwild_snn_benchmark.py --device cpu --n_threads 24 --batch_size 128 --rate_coding
+python hogwild_snn_benchmark.py --device cpu --n_threads 28 --batch_size 128 --rate_coding
+python hogwild_snn_benchmark.py --device cpu --n_threads 32 --batch_size 128 --rate_coding
+python hogwild_snn_benchmark.py --device cpu --n_threads 36 --batch_size 128 --rate_coding
+python hogwild_snn_benchmark.py --device cpu --n_threads 40 --batch_size 128 --rate_coding
 
-python benchmark.py --device cpu --n_threads 0 --batch_size 128 --temporal_coding
-python benchmark.py --device cpu --n_threads 4 --batch_size 128 --temporal_coding
-python benchmark.py --device cpu --n_threads 8 --batch_size 128 --temporal_coding
-python benchmark.py --device cpu --n_threads 12 --batch_size 128 --temporal_coding
-python benchmark.py --device cpu --n_threads 16 --batch_size 128 --temporal_coding
-python benchmark.py --device cpu --n_threads 20 --batch_size 128 --temporal_coding
-python benchmark.py --device cpu --n_threads 24 --batch_size 128 --temporal_coding
-python benchmark.py --device cpu --n_threads 28 --batch_size 128 --temporal_coding
-python benchmark.py --device cpu --n_threads 32 --batch_size 128 --temporal_coding
-python benchmark.py --device cpu --n_threads 36 --batch_size 128 --temporal_coding
-python benchmark.py --device cpu --n_threads 40 --batch_size 128 --temporal_coding
+python hogwild_snn_benchmark.py --device cpu --n_threads 0 --batch_size 128 --temporal_coding
+python hogwild_snn_benchmark.py --device cpu --n_threads 4 --batch_size 128 --temporal_coding
+python hogwild_snn_benchmark.py --device cpu --n_threads 8 --batch_size 128 --temporal_coding
+python hogwild_snn_benchmark.py --device cpu --n_threads 12 --batch_size 128 --temporal_coding
+python hogwild_snn_benchmark.py --device cpu --n_threads 16 --batch_size 128 --temporal_coding
+python hogwild_snn_benchmark.py --device cpu --n_threads 20 --batch_size 128 --temporal_coding
+python hogwild_snn_benchmark.py --device cpu --n_threads 24 --batch_size 128 --temporal_coding
+python hogwild_snn_benchmark.py --device cpu --n_threads 28 --batch_size 128 --temporal_coding
+python hogwild_snn_benchmark.py --device cpu --n_threads 32 --batch_size 128 --temporal_coding
+python hogwild_snn_benchmark.py --device cpu --n_threads 36 --batch_size 128 --temporal_coding
+python hogwild_snn_benchmark.py --device cpu --n_threads 40 --batch_size 128 --temporal_coding
