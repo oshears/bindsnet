b28749d44e620b5cb24245f1ccfed33aaacd6934
oshears <oshears@vt.edu>
2020-12-10 08:39:17 -0500

    minor changes

--- examples/multithread/debug/threaded_mm.py
+++ examples/multithread/debug/threaded_mm.py
@@ -15,14 +15,24 @@ def mmThread():
         y = task["y"]
         z = x @ y
         results.put({"i":i,"z":z})
-
+    
 if __name__ == '__main__':
     threads = []
     n_threads = os.cpu_count()
 
+    # max_size = n_threads * 10000
+    # min_size = n_threads * 100
+    # step_size = n_threads * 1000
     max_size = n_threads * 10000
-    min_size = n_threads * 100
+    min_size = n_threads * 1
     step_size = n_threads * 1000
+
+    iterations = 10
+
+    for _ in range(n_threads):
+        t = Thread(target=mmThread,daemon=True)
+        t.start()
+
     for size in range(min_size,max_size,step_size):
         per_thread = int(size / n_threads)
 
@@ -30,31 +40,37 @@ if __name__ == '__main__':
         y = torch.rand((size,size))
         z = torch.zeros((1,size))
 
-        for t in range(n_threads):
-            t = Thread(target=mmThread,daemon=True)
-            t.start()
+        t0 = 0
+        t1 = 0
 
-        start = time.perf_counter()
-        for i in range(n_threads):
-            item = {}
-            item["x"] = x
-            item["y"] = y[:,i*(per_thread):(i+1)*(per_thread)]
-            item["i"] = i
-            tasks.put(item)
+        for iter in range(iterations):
+            
+            start = time.perf_counter()
+            for i in range(n_threads):
+                item = {}
+                item["x"] = x
+                item["y"] = y[:,i*(per_thread):(i+1)*(per_thread)]
+                item["i"] = i
+                tasks.put(item)
+            
+            for _ in range(n_threads):
+                result = results.get()
+                i = result["i"]
+                z[:,i*per_thread : (i+1)*per_thread] = result["z"]
+
+            t0 += time.perf_counter() - start
+            # print("Threads:",n_threads,"Size",size,"Duration(T): ",end)
+            # print(z)
+
+            start = time.perf_counter()
+            z = x @ y
+            t1 += time.perf_counter() - start
+            # print("Threads:",n_threads,"\tSize:",size,"\tDuration:",end)
+            # print(z)
         
-        for _ in range(n_threads):
-            result = results.get()
-            i = result["i"]
-            z = result["z"]
-            z[:,i*per_thread:(i+1)*per_thread]
+        t0 /= iterations
+        t1 /= iterations
 
-        t0 = time.perf_counter() - start
-        # print("Threads:",n_threads,"Size",size,"Duration(T): ",end)
-
-        start = time.perf_counter()
-        z = x @ y
-        t1 = time.perf_counter() - start
-        # print("Threads:",n_threads,"\tSize:",size,"\tDuration:",end)
         print("Size:",size,"\tSpeedup:",(t1/t0))
         
 
